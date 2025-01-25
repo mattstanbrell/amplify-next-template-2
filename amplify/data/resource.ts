e6dfc45acd8fc7f0d88e76d399c4563e6c386eec
Matt Stanbrell <mattstanbrell@gmail.com>
2025-01-25 17:06:28 +0000

    only owner can see todos

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -12,7 +12,7 @@ const schema = a.schema({
 		.model({
 			content: a.string(),
 		})
-		.authorization((allow) => [allow.authenticated()]),
+		.authorization((allow) => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
