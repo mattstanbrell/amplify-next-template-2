b3ee22708719390e4dfc5f55e931feecc9ba5fdf
Matt Stanbrell <mattstanbrell@gmail.com>
2025-01-28 02:19:43 +0000

    better govuk asset handling

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
 	sassOptions: {
 		includePaths: [path.join(__dirname, "app")],
 	},
+	async rewrites() {
+		return [
+			{
+				source: "/govuk-assets/:path*",
+				destination: "/node_modules/govuk-frontend/dist/govuk/assets/:path*",
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
