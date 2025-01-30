a033f7412c64bcd65fe63bb689cb57163ca45c57
Matt Stanbrell <mattstanbrell@gmail.com>
2025-01-30 04:44:50 +0000

    doesn't work on my machine

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,16 +1,18 @@
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
+import { data } from "./data/resource";
 import { analyseReceiptGemini } from "./functions/analyse-receipt-gemini/resource";
 import { analyseReceiptQwen72b } from "./functions/analyse-receipt-qwen72b/resource";
 import { analyseReceiptQwen7b } from "./functions/analyse-receipt-qwen7b/resource";
-import { analyseReceiptClaude } from "./functions/analyse-receipt-claude/resource";
+// import { analyseReceiptClaude } from "./functions/analyse-receipt-claude/resource";
 import { analyseReceiptGpt4 } from "./functions/analyse-receipt-gpt4/resource";
 
 export const backend = defineBackend({
 	auth,
+	data,
 	analyseReceiptGemini,
 	analyseReceiptQwen72b,
 	analyseReceiptQwen7b,
-	analyseReceiptClaude,
+	// analyseReceiptClaude,
 	analyseReceiptGpt4,
 });
